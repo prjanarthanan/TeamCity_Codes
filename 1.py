b7b1a10c911a2f988207228b12e4beb2479acfe1
PRJanarthanan <prjanarthanan@gmail.com>
2021-08-30 11:54:54 +0530

    adding the file creation line updated

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 print("Hello Jana")
 print("Trying for second update")
-with open('File_create','w') as f:
+with open('File_create.txt','w') as f:
     f.write("I am making a file")
 print("file creation completed")
